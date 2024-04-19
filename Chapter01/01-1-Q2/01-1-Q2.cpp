ae1de343b466cfb0d285e249a00bec1545520fc6
annzy <143188171+rlozlr@users.noreply.github.com>
2024-04-20 01:12:04 +0900

    Chapter01 문제01-1 풀이

---
+++ Chapter01/01-1-Q2/01-1-Q2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+/*
+프로그램 사용자로부터 이름과 전화번호를 문자열의 형태로 입력 받아서,
+입력 받은 데이터를 그대로 출력하는 프로그램을 작성
+*/
+
+int main()
+{
+    char name[100];
+    char mobile[100];
+
+    std::cout << "이름 : ";
+    std::cin >> name;
+    std::cout << "전화번호 : ";
+    std::cin >> mobile;
+
+    std::cout << "\n[사용자 정보]\n";
+    std::cout << "이름 : " << name << std::endl;
+    std::cout << "전화번호 : " << mobile << std::endl;
+}
---
+++ Chapter01/01-1-Q3/01-1-Q3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// 숫자를 하나 입력 받아서 그 숫자에 해당하는 구구단을 출력 
+
+int main()
+{
+	int num;
+	int i = 0;
+	cout << "출력할 구구단은 몇 단? (숫자 입력) >>> ";
+	cin >> num;
+	cout << endl;
+
+	cout << "[" << num << "단]" << endl;
+	for (int i = 1; i <= 9; i++) {
+		cout << num << " X " << i << " = " << num * i << endl;
+	}
+}
